042b726abea5c201d5b80c1444b88abc07f9594c
sy2657 <60480017+sy2657@users.noreply.github.com>
2021-03-14 22:23:09 -0700

    Update svm_polynomial_best_degree.R

--- lib/svm_polynomial_best_degree.R
+++ lib/svm_polynomial_best_degree.R
@@ -11,9 +11,13 @@ svm_polynomial_best_degree <- function(dat_train, K) {
     ypred <- predict(sclass, testset) 
     tab_svm <- table(ypred, dat_test$label)
     n1 <- tab_svm[1]
+    n2 <- tab_svm[2]
+    n3 <- tab_svm[3]
     n4 <- tab_svm[4]
     num <- n1+n4
-    cv.acc[i] <- num/600
+    den <- n1+n2+n3+n4
+    acc <- num/den
+    cv.acc[i] <- acc
 
   }
   return cv.acc # choose the k with max accuracy
